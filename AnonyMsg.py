29d099ccc606edced472eeb86a27ab56e61998a1
OseidAldary(Joker11) <oseid.aldary@gmail.com>
2018-11-18 12:33:57 +0200

    Update AnonyMsg.py

--- AnonyMsg.py
+++ AnonyMsg.py
@@ -26,7 +26,7 @@ wi = '\033[1;37m'
 
 ## Random Proxy For Use..:) 
 
-def proxy():
+def proxy(): # Default Proxy List
 
 	p1 = "185.53.179.29:80" #netherlands
         p2 = "162.255.119.250:80" #US
@@ -61,13 +61,13 @@ def check():
 ## Tool Banner
 banner = gr+'''
   ___                         ___  ___          
- / _ \       @Anonymousl      |  \/  |          
+ / _ \       @Anonymously     |  \/  |          
 / /_\ \_ __   ___  _ __  _   _| .  . |___  __ _ 
 |  _  | '_ \ / _ \| '_ \| | | | |\/| / __|/ _` |
 | | | | | | | (_) | | | | |_| | |  | \__ \ (_| |
 \_| |_/_| |_|\___/|_| |_|\__, \_|  |_/___/\__, |
                           __/ |            __/ |
-Send Anonymously Email:) |___/            |___/ 
+Send Anonymously Email:) |___/ by:JOKER11 |___/ 
 ================================================
 '''
 ###
@@ -75,28 +75,37 @@ Send Anonymously Email:) |___/            |___/
 ### Usage Msg
 parse = optparse.OptionParser(banner+wi+'''
 Usage: python ./AnonyMsg.py [OPTIONS...]
-
+----------------------------------------
 OPTIONS:
-	-r --recipient ::> recipient address?
-
-	-s --subject   ::> subject Of Email?
-
-	-m --message   ::> set your message
-
-	-p --use-porxy ::> If You Want Use Proxy With Sent The Email
+       |
+    |--------
+    | -r --recipient ::> recipient address?
+    |--------
+    | -s --subject   ::> subject Of Email?
+    |--------
+    | -m --message   ::> set your message
+    |--------
+    | -d --def-porxy ::> Use Random Proxy From Default Script Proxy list
+    |--------
+    | -u --use-proxy ::> Use Select Porixy From User
+-------------	
 EXAMPLES:
-
-./AnonyMsg.py -r oseid@gmail.com -s Hacking -m 'Your Have Been Hacked!'
-./AnonyMsg.py -r oseid@gmail.com -s Hacking -m 'Your Have Been Hacked!' -p
-
-
+        |
+     |----------
+     | python AnonyMsg.py -r oseid@gmail.com -s Hacking -m 'Your Have Been Hacked!'
+     |----------
+     | python AnonyMsg.py -r oseid@gmail.com -s Hacking -m 'Your Network is Hacked!' --def-proxy
+     |----------
+     | python AnonyMsg.py -r oseid@gmail.com -s Hacking -m 'Your Server Is Hacked' -u 31.155.3.222:80
+     |----------
 ''')
 ## Create Main Function..:)
 def Main():
 	parse.add_option('-r','--recipient',dest='recaddr',type='string')
 	parse.add_option('-s','--subject',dest='subject',type='string')
         parse.add_option('-m','--message',dest='msg',type='string')
-	parse.add_option('-p','--use-proxy',action='store_true',dest='prox',default=False)
+	parse.add_option('-d','--def-proxy',action='store_true',dest='prox',default=False)
+	parse.add_option("-u","--use-proxy",dest="proxyis",type="string")
 
 	(options,args) = parse.parse_args()
 
@@ -120,7 +129,26 @@ def Main():
                  br.addheaders = [('User-agent', useuser)]
 		 if useproxy == True:
 			br.set_proxies({"https":randproxy})
-                 br.open(url)
+	         elif options.proxyis !=None:
+                    proxyis = options.proxyis
+                    print(wi+"\n["+yl+"~"+wi+"]"+gr+" Connecting To Proxy["+wi+proxyis+gr+"]...")
+                    if ":" in proxyis:
+                       proxy = proxyis.split(":")
+                       proxy,port = proxy[0],int(proxy[1])
+                       try:
+                          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                          s.settimeout(5)
+                          s.connect((proxy, port))
+                          s.close()
+                          br.set_proxies({"https":proxyis})
+                       except socket.error:
+                           print(rd+"\n["+yl+"!"+rd+"]"+yl+" Connection Failed TO Proxy[ "+rd+proxy+":"+str(port)+yl+" ]"+rd+" !!!")
+                           print(wi+"["+yl+"!"+wi+"]"+yl+" Please Try Other Proxy\n"+wi+"["+yl+"!"+wi+"]"+yl+" Select The Proxy Use Port"+wi+"["+yl+"80"+wi+"]"+yl+" And "+wi+"["+yl+"HTTPS"+wi+"]"+yl+" Is "+gr+"Active")
+                           exit(1)
+                    else:
+                        print(wi+"\n["+yl+"!"+wi+"]"+yl+" Please Set A Proxy With Port Like This"+wi+"[ "+yl+"31.155.3.222:80"+wi+" ]")
+                        exit(1)
+                 print(wi+"["+gr+"+"+wi+"]"+gr+"Connected :)")
                  br.set_handle_equiv(True)
                  br.set_handle_gzip(True)
                  br.set_handle_redirect(True)
@@ -128,6 +156,7 @@ def Main():
                  br.set_handle_robots(False)
                  br.set_debug_http(False)
                  br.set_debug_redirects(False)
+                 br.open(url)
 		 br.select_form(nr=0)
 		 br.form['to'] = to
 		 br.form['subject'] = sub
@@ -135,9 +164,12 @@ def Main():
 		 result = br.submit()
 		 response = br.response().read()
 		 if useproxy == True:
-			proxystatus = yl + randproxy +rd+'['+gr+'ON'+rd+']'
+			proxystatus = gr + randproxy +wi+'['+gr+'ON'+wi+']'
+	         elif options.proxyis !=None:
+                    proxyis = options.proxyis
+                    proxystatus = gr + proxyis +wi+'['+gr+'ON'+wi+']'
 		 else:
-			proxystatus = '['+rd+'OFF'+wi+']'
+			proxystatus = yl+'['+rd+'OFF'+yl+']'
 		 print(' ')
 		 print(rd+'='*10+gr+'> Config <'+rd+'='*10)
 		 print(wi+"[R] RECIPIENT    :> "+gr+to)
@@ -149,14 +181,14 @@ def Main():
 		 print(wi+"[P] ProxyStatus  :> "+proxystatus)
 		 print(rd+"="*30)
 		 sleep(0.30)
-		 print(yl+'\n[$]'+wi+'Sending Msg To[ '+rd+to+wi+' ] '+rd+'....')
+		 print(gr+'\n['+yl+'~'+gr+'] '+wi+'Sending Msg To[ '+yl+to+wi+' ] '+gr+'...')
 		 sleep(2.5)
    		 if "The e-mail has been sent anonymously!" in response:
-			print(gr+"\n[#]"+yl+" The Email Has Been Sent Anonymously TO[ "+rd+to+yl+" ] :)")
-			print(gr+'[>]'+wi+' The Recipient Will Get The Email In Up To 12 Hours')
+			print(gr+"\n["+wi+"#"+gr+"]"+yl+" The Email Has Been Sent Anonymously TO[ "+gr+to+yl+" ] :)")
+			print(gr+'['+wi+'>'+gr+']'+wi+' The Recipient Will Get The Email In Up To 12 Hours')
 		 else:
-		      print(rd+'\n[x] '+wi+"Failed To Send Email To[ "+yl+to+wi+" ] :(")
-		      print(rd+'[#] '+gr+'Try Again Later![ OR ] Try[ '+yl+'-p '+gr+' ] To Use Proxy :)')
+		      print(rd+'\n['+yl+'!'+rd+'] '+yl+"Failed To Send Email To[ "+rd+to+yl+" ]"+rd+" :(!")
+		      print(rd+'['+yl+'!'+rd+'] '+yl+'Try Again Later!'+rd+'[ '+yl+'OR'+rd+' ] '+wi+'Using '+gr+'['+wi+'Proxy'+gr+']'+wi+' OPTIONS')	      
 
                 else:
 		     print(yl+"\n[!] "+gr+'Error Your Not Connected To [ '+rd+'INTERNET'+gr+' ]!\n[#] Please Connect To Internet And Try Again :)')
@@ -168,8 +200,6 @@ def Main():
 
 if __name__=='__main__':
 	Main()
-
-
 ##############################################################
 ##################### 		     #########################
 #####################   END OF TOOL  #########################
@@ -178,4 +208,3 @@ if __name__=='__main__':
 #This Tool by Oseid Aldary
 #Have a nice day :)
 #GoodBye
-
